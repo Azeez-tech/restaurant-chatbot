38a5a793ecb79f66d5ffccc55574a309506247fb
Azeez muhammed <muhammedazeez09@gmail.com>
2025-04-16 21:11:32 +0100

    cookie

--- server.js
+++ server.js
@@ -5,10 +5,11 @@ const axios = require("axios");
 require("dotenv").config();
 const MongoStore = require("connect-mongo");
 const cors = require("cors");
+const cookieParser = require("cookie-parser");
 
 const app = express();
 const PORT = process.env.PORT || 3000;
-
+app.use(cookieParser());
 // Enable CORS for all origins for simplicity (adjust as needed)
 app.use(cors({ credentials: true, origin: true }));
 app.use(express.json());
